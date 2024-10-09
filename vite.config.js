2549ec4feccd9295336e3a1a214bc284bcc788c8
Sudharshan <sudharshanj123@gmail.com>
2024-10-09 21:25:36 +0530

    created .env and .env.production and link the baseUrl to proxy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( ({mode}) => {
+  // Load environment variables based on the mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8081', // Backend server URL
+        target: env.VITE_API_BASE_URL, // Backend server URL
         changeOrigin: true,               // Changes the origin of the host header to the target URL
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api from the path
       },
     },
   },
-})
+}
+});
+
+
