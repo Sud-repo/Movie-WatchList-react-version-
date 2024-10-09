c3fd2e0a4fdf84a66d1f3735831ce1b8487be835
Sudharshan <sudharshanj123@gmail.com>
2024-10-09 18:57:47 +0530

    updated movies list to fetch from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CardsContainer from "../components/CardsContainer";
-import movies from "../components/Movies";
+import jsonMovies from "../components/Movies";
 import Empty from '../components/Empty';
+import Loader from '../components/Loader';
 
 
 function HomePage() {
+
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect( () => {
+    async function fetchMovies() {
+      try{
+        const res = await fetch('http://localhost:8081/movies');
+        const jsonRes = await res.json();
+        setMovies(jsonRes.data);
+      } catch (error) {
+        console.log('Error fetching Data: ' +error);
+        setMovies(jsonMovies);
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchMovies();
+  }, [])
 
   let isMovies = movies.length === 0 ? false : true;
 
