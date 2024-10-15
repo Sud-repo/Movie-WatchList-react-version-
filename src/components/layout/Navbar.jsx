1485a2abaca588a077874ddb7a0fe75a2b910f4d
Sudharshan <sudharshanj123@gmail.com>
2024-10-15 19:18:38 +0530

    updated navbar-toggler to close on location change

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,30 @@
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useRef } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Navbar() {
 
 	const linkStyle = ({ isActive }) => isActive ? "nav-link active-link" : "nav-link";
 
+	const location = useLocation();
+	const navbarTogglerRef = useRef(null);
+
+	const closeNavbar = () => {
+		const navbarCollapse = document.getElementById('navbarNav');
+		if (navbarCollapse.classList.contains('show')) {
+		navbarTogglerRef.current.click();
+		}
+	};
+
+	useEffect(() => {
+		closeNavbar();
+	}, [location]);
+
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark main-header">
 		{/* <a className="navbar-brand nav-title" href="/movies/list">{ props.title }</a> */}
 		<button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-			aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" ref={navbarTogglerRef}>
 			<span className="navbar-toggler-icon"></span>
 		</button>
 		<div className="collapse navbar-collapse nav-align" id="navbarNav">
