1b54d9e1451891951feb44111546d937951a89ec
Sudharshan <sudharshanj123@gmail.com>
2024-10-18 14:05:19 +0530

    updated buttons style

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 function NotFoundPage() {
   return (
     <div className='not-found'>
-        <h1 style={{fontSize: '10rem', margin: '0px'}} >404</h1>
-        <h2>Page Not Found <i className="fa-solid fa-triangle-exclamation"></i> </h2>
+        <h1 style={{fontSize: '10rem', margin: '0px'}} >404!</h1>
+        <h2>Page Not Found <i className="fa-solid fa-triangle-exclamation fa-beat"></i> </h2>
         <p className='nf-p'>The page you are looking for does not exist.<br /> 
         How you got here is a mystery.<br />
         But you can click the button below to go back to the homepage.</p>
