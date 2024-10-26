19a932db54a144cafce573220e805cda2cdd25b6
Sudharshan <sudharshanj123@gmail.com>
2024-10-26 22:35:00 +0530

    updated new Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,33 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 function Loader({ show }) {
   const [isVisible, setIsVisible] = useState(false);
-  const [opacity, setOpacity] = useState(0);
   
   useEffect(() => {
     if (show) {
       setIsVisible(true);
-      setOpacity(0.6);
     } else {
-      setOpacity(0);
+      setIsVisible(false);
     }
   }, [show]);
 
-  const handleTransitionEnd = () => {
-    if (!show) {
-      setIsVisible(false);
-    }
-  };
-
-  const loaderStyle = {
-    opacity: opacity,
-    transition: 'opacity 0.5s ease-in-out',
-    display: isVisible ? 'flex' : 'none'
-  };
 
   return (
-    <div className="loader-bg" style={loaderStyle} onTransitionEnd={handleTransitionEnd}>
-		<div className="loader"></div>
-	</div>
+		<div className="loader" style={{display: isVisible ? 'grid' : 'none'}}></div>
   )
 }
 
