37fa82245fa69f8f25dceb75c1c9346128823fa9
Sudharshan <sudharshanj123@gmail.com>
2024-11-04 22:28:34 +0530

    added react-infinite-scroll-component dependency and created infinite scroll for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,53 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import CardsContainer from "../components/movie/CardsContainer";
 import Empty from '../components/movie/Empty';
 import Loader from '../components/Loader'
+import MoiveLoader from '../components/MoiveLoader';
 
 
 function HomePage() {
 
-  const [movies, setMovies] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const hasMore = useRef(true);
+  const pageNo = useRef(0);
+  const initialFetchDone = useRef(false);
 
-  useEffect( () => {
-    async function fetchMovies() {
-      try{
-        const res = await fetch('/base-url/movies');
-        if (!res.ok) throw new Error(`Status - ${res.status} ${res.statusText}`);
-        const jsonRes = await res.json();
-        setMovies(jsonRes.data);
-      } catch (error) {
-        console.error(error);
-        setMovies([]);
-      } 
+  useEffect(() => {
+    if (!initialFetchDone.current || pageNo.current !== 0) {
+      fetchMovies();
+      initialFetchDone.current = true;
     }
+  }, []);
 
-    fetchMovies();
-  }, [])
+  async function fetchMovies() {
+    try {
+      const res = await fetch(`/base-url/movies?page-no=${pageNo.current}`);
+      if (!res.ok) throw new Error(`Status - ${res.status} ${res.statusText}`);
+      const jsonRes = await res.json();
+      if (jsonRes.isLast) hasMore.current =false;
+      if (jsonRes.data !== null) setMovies((prevMovies) => [...prevMovies, ...jsonRes.data]);
+      pageNo.current = pageNo.current+1;
+    } catch (error) {
+      console.error(error);
+      hasMore.current =false;
+    }
+  }
 
   return (
     <>
-      {movies === null ? <> <div className='Empty-space'></div> <Loader /> </> : movies.length > 0 ? (
-        <CardsContainer movies={movies} isHome={true} />
+      {movies.length === 0 && initialFetchDone ? <> 
+          <div className='Empty-space'></div> <Loader /> 
+        </> : movies.length > 0 ? (
+          <InfiniteScroll
+          dataLength={movies.length}
+          next={fetchMovies}
+          hasMore={hasMore.current}
+          loader={<MoiveLoader />}
+          endMessage={<div>End</div>}
+        >
+          <CardsContainer movies={movies} isHome={true} />
+        </InfiniteScroll>
       ) : (
         <Empty />
       )}
