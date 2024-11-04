ea67f9ae437506ec8469424e2b10b6d9ab3b862f
Sudharshan <sudharshanj123@gmail.com>
2024-11-04 22:29:30 +0530

    Created MovieLoader for infiniteScroll loader

---
+++ src/components/MoiveLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MoiveLoader() {
+  return (
+    <div className="movie-loader">
+        <i className="fa-solid fa-compact-disc fa-spin-pulse"></i>
+    </div>
+  )
+}
+
+export default React.memo(MoiveLoader);
